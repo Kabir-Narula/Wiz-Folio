6482a0272f7043269c5c055aca3a5bf698f11843
Kabir <AckerLevi102@gmail.com>
2024-03-18 19:55:02 -0400

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,124 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+
+  {
+    id: 1,
+    name: "XO SPACETECH",
+    description: "Design Your House in 3d Enviornment (In progress)",
+    date: "2024-03-10",
+    demoLink: "https://github.com/Kabir-Narula/XO-SPACETECH",
+  },
+  {
+    id: 2,
+    name: "Comic Portfolio",
+    description: "Portfolio Dedicated To Dazai",
+    date: "2024-02-12",
+    demoLink: "https://portfolio-comic.vercel.app",
+  },
+  {
+    id: 3,
+    name: "Aespa",
+    description: "Meet The Members",
+    date: "2024-02-21",
+    demoLink: "https://kabir-narula.github.io/Aespa/",
+  },
+  {
+    id: 4,
+    name: "LegoSets",
+    description: "Choose Your Desired Lego Collection",
+    date: "2023-11-30",
+    demoLink: "https://github.com/Kabir-Narula/WEB322-LegoSets",
+  },
+  {
+    id: 5,
+    name: "Lesserafim",
+    description: "Meet the Fearless members",
+    date: "2023-11-12",
+    demoLink: "https://odd-cyan-butterfly-shoe.cyclic.app",
+  },
+  {
+    id: 6,
+    name: "AssemblyLine-Simulator",
+    description: "Terminal Based Simulator To Process Orders",
+    date: "2023-11-26",
+    demoLink: "https://github.com/Kabir-Narula/CPP-AssemblyLine-Simulator",
+  },
+  {
+    id: 7,
+    name: "Vintage Music",
+    description: "A Collection Of Hand-Picked Artists",
+    date: "2023-06-02",
+    demoLink: "https://kabir-narula.github.io/WEB222-Vintage/",
+  },
+  {
+    id: 8,
+    name: "Seneca Library",
+    description: "Terminal Based Library Application",
+    date: "2023-06-03",
+    demoLink: "https://github.com/Kabir-Narula/Seneca-Library-Application",
+  },
+  {
+    id: 9,
+    name: "Veterinarian Clinic System",
+    description: "Terminal Based Clinic System",
+    date: "2023-08-22",
+    demoLink: "https://github.com/Kabir-Narula/Veterinarian-Clinic-System",
+  },
+  {
+    id: 10,
+    name: "The Odin Project",
+    description: "Dedicated Side-Projects",
+    date: "2023-02-14",
+    demoLink: "https://github.com/Kabir-Narula/The-Odin-Project",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Kabir-Narula",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/kabir-narula-19b129260/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.linkedin.com/in/kabir-narula-19b129260/",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Unmatched RIzz
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            My journey in web development has been crazy ups and down
+            I started with html and css, thinking its not for me but
+            it was nt later when i discovered the magic of React and 
+            its Componenets.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-4xl">
+            Rizzler University <sub className="font-semibold text-base">Keep your girl safe ....</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            2+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=Kabir-Narula&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Kabir"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=Kabir-Narula&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Kabir"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="Kabir"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=Kabir-Narula&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt=""
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/Kabir-Narula/Aespa"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=Kabir-Narula&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+              alt="CodeBucks"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
